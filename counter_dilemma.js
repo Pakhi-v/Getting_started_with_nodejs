e3c6a9c62a7f283a871fbc6eea67646e22c93fed
pakhi <pakhivashis182001@gmail.com>
2022-12-15 18:32:14 +0530

    counter dilemma in js

---
+++ counter_dilemma.js
@@ -0,0 +1,12 @@
+const add = (function () {
+    let counter = 0;
+    function inner(){
+        counter=counter+1;
+        return counter;
+    }
+    return inner;
+  }) ();
+  
+  add();
+  add();
+  console.log(add());
